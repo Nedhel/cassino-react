fd74c4bb8f39d8a1dbf4b01d214da746b619c3dd
Carlos Zalpa <nedhel.90@gmail.com>
2023-08-24 01:00:27 -0700

    roulette added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            gridTemplateColumns: {
+                // Simple 13 column grid
+                13: "repeat(13, minmax(0, 1fr))",
+            },
+        },
     },
     plugins: [],
 };
